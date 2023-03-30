b7ae0f5f505e42f61005e1c01d0d83abc95b5819
cemb4 <cemb4@yandex.ru>
2023-03-30 19:37:39 +0300

    cemb4

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,16 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+
+        Cat cat1 = (Cat) applicationContext.getBean("cat");
+        Cat cat2 = (Cat) applicationContext.getBean("cat");
+
+
+        System.out.println(bean.equals(bean1));
+        System.out.println(cat1.equals(cat2));
         System.out.println(bean.getMessage());
+        System.out.println(bean1.getMessage());
     }
 }
